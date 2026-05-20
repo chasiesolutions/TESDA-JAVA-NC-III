48857b253fa5b28b53475fddc1ae866fc7db6f86
chasiesolutions <chasiesolutions@gmail.com>
2026-05-20 11:57:32 +0800

    Create task2.java

---
+++ chapter1/task2.java
@@ -0,0 +1,18 @@
+public class Task2 {
+    public static void main(String[] args) {
+        char letterH = 'H';
+        char letterR = 'r';
+        char letterD = 'd';
+        byte zero = 0;
+        short eleven4doubleL = 11;
+        int thee = 3;
+        int one4L = 1;
+        float version = 2.0f;
+        boolean flag = true;
+        System.out.println("" + letterH + three + eleven4doubleL + zero + 
+                           " " + "w" + zero + letterR + one4L + letterD + 
+                           " " + version + " " + flag);
+      String output = "W" + zero + "w"; 
+        System.out.println(output);
+    }
+}
