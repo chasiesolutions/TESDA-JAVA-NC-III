7a31d0634f9bc869a9fec2fb78d222764403cbd4
chasiesolutions <chasiesolutions@gmail.com>
2026-05-21 08:11:19 +0000

    Fix

--- Main.java
+++ Main.java
@@ -1,1 +1,19 @@
+/**
+ * @author Pat Mercado based on Marco Yimyaem's training class
+ * Toy Object
+ * has a property of name, brand, price, and quantity
+ * whith a method of setPrice() where you can change 
+ * the price state of Toy Object.
+ */
+package chapter1;
 
+pubic class Main{
+    public static void main(String[] args) {
+        Toy toy1 = new Toy();
+        toy1.brand = "";
+        toy1.name = "";
+        toy1.price = 0;
+        toy1.quantity = 12;
+        
+    }
+}
---
+++ Task2.java
@@ -0,0 +1,18 @@
+public class Task2 {
+    public static void main(String[] args) {
+        char letterH = 'H';
+        char letterD = 'd';
+        int three = 3;
+        short eleven4LL = 11;
+        byte zero = 0;
+        int one4L = 1;
+        float version = 2.0f;
+        boolean flag = true;
+
+        String output = ("" + letterH + three + eleven4LL + zero 
+                + " w" + zero + "r" + one4L + letterD 
+                + " " + version + " " + flag);
+        System.out.println(output);
+        // H3110 w0r1d 2.0 true
+    }
+}
