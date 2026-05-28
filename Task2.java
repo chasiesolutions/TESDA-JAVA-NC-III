589efce54aaa4c1b8214bb489ea00314bfc4b121
chasiesolutions <chasiesolutions@gmail.com>
2026-05-28 01:36:28 +0000

    Updated 202605280939

--- Task2.java
+++ Task2.java
@@ -17,3 +17,11 @@ public class Task2 {
         // H3110 w0r1d 2.0 true
     }
 }
+1: public class WaterBottle {
+2: private String brand;
+3: private boolean empty;
+4: public static void main(String[] args) {
+5:        WaterBottle wb = new WaterBottle();
+6:        System.out.print("Empty = " + wb.empty);
+7:        System.out.print(", Brand = " + wb.brand);
+8: } }
