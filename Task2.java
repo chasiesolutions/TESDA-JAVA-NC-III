cf4fc11a3cd494b7ece416b8f2beae5588e20956
chasiesolutions <chasiesolutions@gmail.com>
2026-06-12 14:14:08 +0000

    //202606122214

--- Task2.java
+++ Task2.java
@@ -1,19 +1,22 @@
-// For submission Updated 20260528 B
+// For submission Updated 20260528
+/* Create all of the primitives (except long and double) with 
+different values. Concatenate them into a string and print it to
+the screen so it will print:
+H3110 w0rld 2.0 true
+*/
+package otherFolder;
 public class Task2 {
+
     public static void main(String[] args) {
-        char letterH = 'H';
-        char letterD = 'd';
-        int three = 3;
+        char letterH = 'H', letterD = 'd';
+        int three = 3, one4L = 1;
         short eleven4LL = 11;
         byte zero = 0;
-        int one4L = 1;
         float version = 2.0f;
         boolean flag = true;
-
         String output = ("" + letterH + three + eleven4LL + zero 
                 + " w" + zero + "r" + one4L + letterD 
                 + " " + version + " " + flag);
         System.out.println(output);
-        // H3110 w0r1d 2.0 true
     }
 }
