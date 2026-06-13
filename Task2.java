748c7ea6098f458c5f0310f9ca5dd762b60a6aa8
chasiesolutions <chasiesolutions@gmail.com>
2026-06-13 02:56:43 +0000

    202606131056

--- Task2.java
+++ Task2.java
@@ -4,16 +4,16 @@ different values. Concatenate them into a string and print it to
 the screen so it will print:
 H3110 w0rld 2.0 true
 */
-package otherFolder;
-public class Task2 {
 
-    public static void main(String[] args) {
+public class Task2 {
         char letterH = 'H', letterD = 'd';
         int three = 3, one4L = 1;
         short eleven4LL = 11;
         byte zero = 0;
         float version = 2.0f;
         boolean flag = true;
+    public static void main(String[] args) {
+
         String output = ("" + letterH + three + eleven4LL + zero 
                 + " w" + zero + "r" + one4L + letterD 
                 + " " + version + " " + flag);
