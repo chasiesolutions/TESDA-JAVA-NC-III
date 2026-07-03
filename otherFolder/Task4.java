ab5fa4b8716180319268cf7e9b7f8ab7b1ce0932
chasiesolutions <chasiesolutions@gmail.com>
2026-07-03 00:48:36 +0000

    202607030848

--- otherFolder/Task4.java
+++ otherFolder/Task4.java
@@ -1,15 +1,12 @@
 /*
 Write a program that takes a string as input and checks if it is a palindrome. 
-A palindrome is a word, number, or other sequence of characters that 
-reads the same forward and backward. 
+A palindrome is a word, number, or other sequence of characters that reads the same forward and backward. 
 
 Your program should perform the following steps:
-
-Use StringBuilder to create a new string that is a reverse of the input string.
-Use the equals() method to check if the input string and 
-the reversed string are the same.
-If the strings are the same, print "The input string is a palindrome."
-If the strings are not the same, print "The input string is not a palindrome."
+- Use StringBuilder to create a new string that is a reverse of the input string.
+- Use the equals() method to check if the input string and the reversed string are the same.
+- If the strings are the same, print "The input string is a palindrome."
+- If the strings are not the same, print "The input string is not a palindrome."
 */
 package otherFolder;
 import java.util.*;
